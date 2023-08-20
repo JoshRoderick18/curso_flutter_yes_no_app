c3b29cef889ab54a75f0259a66bac450082aae8c
joshroderick18 <joshroderick18@gmail.com>
2023-08-20 18:26:57 -0400

    Add ScrollController to the ChatProvider

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -2,14 +2,28 @@ import 'package:curso_flutter_yes_no_app/domain/entities/message.dart';
 import 'package:flutter/material.dart';
 
 class ChatProvider extends ChangeNotifier {
+  final ScrollController chatScrollController = ScrollController();
+
   List<Message> messageList = [
     Message(text: "Hola jefe", fromWho: FromWho.me),
     Message(text: "Me soplo viento", fromWho: FromWho.me),
   ];
   Future<void> sendMessage(String text) async {
+    if (text.isEmpty) return;
     final newMessage = Message(text: text, fromWho: FromWho.me);
     messageList.add(newMessage);
 
     notifyListeners();
+    scrollToBottom();
+  }
+
+  Future<void> scrollToBottom() async {
+    await Future.delayed(const Duration(milliseconds: 100));
+
+    chatScrollController.animateTo(
+      chatScrollController.position.maxScrollExtent,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeOut,
+    );
   }
 }
