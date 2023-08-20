92eebb5b1aa816ba2feb21edea8f17f9bfc41335
joshroderick18 <joshroderick18@gmail.com>
2023-08-20 17:31:18 -0400

    Add Chat Provider

---
+++ lib/presentation/providers/chat_provider.dart
@@ -0,0 +1,12 @@
+import 'package:curso_flutter_yes_no_app/domain/entities/message.dart';
+import 'package:flutter/material.dart';
+
+class ChatProvider extends ChangeNotifier {
+  List<Message> message = [
+    Message(text: "Hola jefe", fromWho: FromWho.me),
+    Message(text: "Me soplo viento", fromWho: FromWho.other),
+  ];
+  Future<void> sendMessage(String text) async {
+    // Todo: implementar metodo
+  }
+}
