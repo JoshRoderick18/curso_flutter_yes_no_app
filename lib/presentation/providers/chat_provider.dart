267382590d276d46fe53b1d3530b041d21eef638
joshroderick18 <joshroderick18@gmail.com>
2023-08-20 17:55:11 -0400

    Housekeeping

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -2,7 +2,7 @@ import 'package:curso_flutter_yes_no_app/domain/entities/message.dart';
 import 'package:flutter/material.dart';
 
 class ChatProvider extends ChangeNotifier {
-  List<Message> message = [
+  List<Message> messageList = [
     Message(text: "Hola jefe", fromWho: FromWho.me),
     Message(text: "Me soplo viento", fromWho: FromWho.other),
   ];
